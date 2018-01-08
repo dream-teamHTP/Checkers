6b8d9d718fe4d503be8322ec18e4ef63c401bde8
AlexB <agb88@mail.ru>
2018-01-08 23:51:30 +0300

    Changed matrix creation. Created inheritance.

--- movement.js
+++ movement.js
@@ -6,9 +6,50 @@
 'use strict';
 
 /**
- * Current drag element.
+ * An array that contains number of objects
+ * that contains {parent: constructor, child: constructor}
  */
-let cells = {dragChecker: null};
+let parentChild = [
+    {
+        parent: Cells,
+        child: Cell
+    },
+    {
+        parent: Cell,
+        child: Checker
+    }];
+
+let cells = new Cells();
+
+/**
+ * General object for matrix
+ */
+function Cells() {
+
+    /** Current drag element */
+    this.dragChecker = null;
+
+    /** Sets current checker to the top of all checkers */
+    Cells.prototype.setCheckerToTop = function () {
+        this.element.parent().append(this.element);
+    };
+
+    /** Updates the difference between mouse and element */
+    Cells.prototype.updateDiff = function (event) {
+        this.diffTop = event.clientY - this.offsetTop;
+        this.diffLeft = event.clientX - this.offsetLeft;
+    };
+
+    /**
+     * Updates offset in object after successful drop.
+     * When the next time clicks on this element,
+     * the difference between mouse and element will count from last position
+     */
+    Cells.prototype.updateCheckerOffset = function (event) {
+        this.offsetTop = event.clientY - this.diffTop;
+        this.offsetLeft = event.clientX - this.diffLeft;
+    }
+}
 
 /**
  * An object for every cell.
@@ -25,20 +66,6 @@ function Cell(cell, cellId, offsetLeft, offsetTop) {
     this.id = cellId;
     this.checker = {};
 }
-
-/**
- * Fires all cells for listeners to drop
- */
-function fillCells(check, coordinateY, coordinateX, offsetLeft, offsetTop) {
-    let cellId = "" + coordinateY + coordinateX;
-    let cell = cells[cellId] = new Cell(check, cellId, offsetLeft, offsetTop);
-    cell.element
-        .droppable(droppableOptions);
-}
-
-let droppableOptions = {
-    accept: "cell"
-};
 
 /**
  * An object for every checker.
@@ -63,31 +90,16 @@ function Checker(ches, prefix, id, offsetLeft, offsetTop) {
     this.diffLeft = 0;
     this.isDropped = false;
     this.isQueen = false;
+}
 
-    /**
-     * Sets current checker to the top of all checkers
-     */
-    Checker.prototype.setCheckerToTop = function () {
-        this.element.parent().append(this.element);
-    };
-
-    /**
-     * Updates the difference between mouse and element
-     */
-    Checker.prototype.updateDiff = function (event) {
-        this.diffTop = event.clientY - this.offsetTop;
-        this.diffLeft = event.clientX - this.offsetLeft;
-    };
-
-    /**
-     * Updates offset in object after successful drop.
-     * When the next time clicks on this element,
-     * the difference between mouse and element will count from last position
-     */
-    Checker.prototype.updateCheckerOffset = function (event) {
-        this.offsetTop = event.clientY - this.diffTop;
-        this.offsetLeft = event.clientX - this.diffLeft;
-    }
+/**
+ * Fires all cells for listeners to drop
+ */
+function fillCells(check, coordinateY, coordinateX, offsetLeft, offsetTop) {
+    if (!coordinateY || !coordinateX) return;
+    let cellId = "" + coordinateY + coordinateX;
+    let cell = cells[cellId] = new Cell(check, cellId, offsetLeft, offsetTop);
+    cell.element.droppable(droppableOptions);
 }
 
 /**
@@ -109,6 +121,9 @@ function fillCheckers(ches, prefix, coordinateY, coordinateX, offsetLeft, offset
         .mouseup((event) => {
             checker.updateCheckerOffset(event);
             cells.dragChecker = null;
+        })
+        .click((event)=>{
+
         });
 }
 
@@ -119,9 +134,10 @@ let draggableOptions = {
     addClasses: false,
     scope: "cell",
     stop: function (event, ui) {
+        //var a = cells.dragChecker.element.draggable("option");
         // uses when drag stops
     },
-    drag: (event) => {
+    drag: (event, ui) => {
         let checker = cells.dragChecker;
         checker.offsetLeft = event.clientX - checker.diffLeft;
         checker.offsetTop = event.clientY - checker.diffTop;
@@ -129,3 +145,31 @@ let draggableOptions = {
         checker.element.attr('x', "" + checker.offsetLeft);
     }
 };
+
+let droppableOptions = {
+    //addClasses: false,
+    accept: (event, ui) => {
+        // doesn't work
+    },
+    drop: function (event, ui) {
+        // doesn't work
+    },
+    classes:{
+        "ui-droppable": "reddd"
+    }
+};
+
+/**
+ * Function for creating inheritance
+ * @param parentChild - an array that contains number of objects
+ * that contains {parent: constructor, child: constructor}
+ */
+(function createInheritance(parentChild) {
+    let f = function () {
+    };
+    parentChild.forEach((element) => {
+        f.prototype = element.parent.prototype;
+        element.child.prototype = new f();
+        element.child.prototype.constructor = element.child;
+    });
+})(parentChild);
